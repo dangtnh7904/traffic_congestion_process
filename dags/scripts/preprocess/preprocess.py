4d65b1cbf72aac4b7d7c925a4fb0d22e55a7be64
{Dangg} <shadowquapro@gmail.com>
2025-10-28 14:15:05 +0700

    complete preprocessing data

--- dags/scripts/preprocess/preprocess.py
+++ dags/scripts/preprocess/preprocess.py
@@ -1,24 +1,23 @@
 import json
-
 from dags.schema.traffic_schema import root_schema
+
 def process_traffic_data(**kwargs):
     try:
         from pyspark.sql import SparkSession
-        from pyspark.sql.functions import col, monotonically_increasing_id
+        from pyspark.sql.functions import col, lit, greatest, least, when
     except ImportError as e:
         raise ImportError("PySpark is not installed. Please install PySpark to use this function.") from e
 
     spark = SparkSession.builder.appName("TrafficDataProcessing").getOrCreate()
     ti = kwargs['ti']
 
-    raw_file_path = ti.xcom_pull(task_ids='fetch_hanoi_traffic_data')
-    
-    parsed_df = spark.read.json(raw_file_path).schema(root_schema)
+    #Fetch the s3 path from XCom
+    s3_path = ti.xcom_pull(task_ids='fetch_hanoi_traffic_data')
+    # Read the raw JSON data from S3
+    raw_df = spark.read.json(s3_path, schema=root_schema)
 
-    #first flatten the results array
-    parsed_df = parsed_df.selectExpr("sourceUpdated", "explode(results) as result")
-
-
+    # Flatten the DataFrame
+    parsed_df = raw_df.selectExpr("sourceUpdated", "explode(results) as result")
 
     # Flatten the DataFrame and selecting the required fields
     flattened_df = parsed_df.select(
@@ -33,24 +32,43 @@ def process_traffic_data(**kwargs):
         col("result.currentFlow.traversability").alias("traversability"),
         col("result.currentFlow.freeFlowSpeed").alias("freeFlowSpeed")
     )
-    
+
     #consolidate and clean data
     clean_df = flattened_df\
         .filter(col("speed").isNotNull())\
         .filter(col("speed") > 0)\
-        .filter(col("traverse_length") > 0)
+        .filter(col("traverse_length") > 0)\
+        .filter(col("freeFlowSpeed").isNotNull())\
+        .filter(col("freeFlowSpeed") > 0)
     
+    calculated_jamFactor = greatest(lit(0), 
+        least(lit(10), 
+              10 * (1 - (col("speed") / col("freeFlowSpeed")))
+            )
+    )
+
     #remove null jamFactor and using this function to fill missing jamFactor 
     # JF=min(10,max(0,10×(1−speed/freeFlowSpeed)))
     clean_df = clean_df\
         .withColumn("jamFactor", 
-                    col("jamFactor").when(col("jamFactor").isNull(),
-                    10 * (1 - (col("speed") / col("freeFlowSpeed"))))\
-                    .otherwise(col("jamFactor"))
-                    )
+            when(col("jamFactor").isNull(),
+                calculated_jamFactor  # Use the full, correct formula
+            ).otherwise(col("jamFactor"))
+        )
+    
     #choose only data that has confidence score above threshold
     confidence_threshold = 0.7
     clean_df = clean_df.filter(col("confidence") >= confidence_threshold)
 
-    # saving processed data
-    run_id = kwargs['run_id']
+    #save the cleaned data back to S3 in parquet format
+    output_path = s3_path.replace("raw", "processed").replace(".json", ".parquet")
+    clean_df.write.mode("overwrite").parquet(output_path)
+
+    #check if data is saved correctly
+    try:
+        _ = spark.read.parquet(output_path)
+    except Exception as e:
+        raise IOError(f"Failed to read back the processed data from {output_path}") from e
+    
+    # Return the output path for downstream tasks
+    return output_path
